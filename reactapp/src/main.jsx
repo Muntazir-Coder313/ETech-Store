f6fee86341168f2785df95e5d606a109894e434c
Muntazir-Coder <muntazirma21@gmail.com>
2026-05-25 15:47:02 -0800

    fix vercel 404

--- reactapp/src/main.jsx
+++ reactapp/src/main.jsx
@@ -1,15 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 
-import './index.css'
-import App from './App.jsx'
-import { CartProvider } from './Context/CartContext.jsx'
+import "./index.css";
+import App from "./App.jsx";
 
+import { CartProvider } from "./context/CartContext";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <CartProvider>
-    <App />
+      <App />
     </CartProvider>
-  </StrictMode>,
-)
+  </StrictMode>
+);
