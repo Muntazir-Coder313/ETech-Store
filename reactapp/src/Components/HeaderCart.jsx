30fa3b5cac41296211548c6811a9ab6de2582da1
Muntazir-Coder <muntazirma21@gmail.com>
2026-05-25 18:31:38 -0800

    fix build

--- reactapp/src/Components/HeaderCart.jsx
+++ reactapp/src/Components/HeaderCart.jsx
@@ -1,27 +1,4 @@
-// import { Link } from "react-router-dom";
-// import { useCart } from "../context/CartContext";
-// import { ShoppingCart } from "lucide-react";
 
-// function HeaderCart() {
-//   const { cartCount } = useCart();
-
-//   return (
-//     <Link
-//       to="/cart"
-//       className="relative w-11 h-11 rounded-full bg-gray-100 hover:bg-orange-100 flex items-center justify-center"
-//     >
-//       <ShoppingCart size={20} />
-
-//       {cartCount > 0 && (
-//         <span className="absolute -top-1 -right-1 w-5 h-5 bg-orange-500 text-white text-[10px] flex items-center justify-center rounded-full animate-bounce">
-//           {cartCount}
-//         </span>
-//       )}
-//     </Link>
-//   );
-// }
-
-// export default HeaderCart;
 
 
 // ===========================================
